9b898d01f68cac577eb3392eb71ad0cc0ffa06c7
Remmysson Kristhyan <remmysson.teles@al.senai.br>
2023-01-03 18:16:01 -0300

    Aula 03: Adicionar item na lista

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,8 +1,19 @@
-const novaTarefa = document.querySelector('[data-form-button');
+const criarTarefa = (evento) => {
+    evento.preventDefault();
 
-
-novaTarefa.addEventListener('click', () => {
+    const lista = document.querySelector('[data-list]');
     const input = document.querySelector('[data-form-input]');
     const valor = input.value;
-    console.log(valor);
-})
+
+    const tarefa = document.createElement('li');
+    tarefa.classList.add('task');
+    const conteudo = `<p class="content">${valor}</P>`;
+    tarefa.innerHTML = conteudo;
+    lista.appendChild(tarefa);
+
+    input.value = "";
+} 
+
+const novaTarefa = document.querySelector('[data-form-button');
+
+novaTarefa.addEventListener('click', criarTarefa);
